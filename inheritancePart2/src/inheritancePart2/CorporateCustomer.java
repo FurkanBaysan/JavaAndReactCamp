a443d8b876e90fcea54e39fd9f6d67110a51b661
FurkanBaysan <furkanbaysan1600166@gmail.com>
2023-03-04 18:21:15 +0300

    Polymorphism, Dependency Injection with logging scenario (encapsulation support added)

--- inheritancePart2/src/inheritancePart2/CorporateCustomer.java
+++ inheritancePart2/src/inheritancePart2/CorporateCustomer.java
@@ -3,4 +3,20 @@ package inheritancePart2;
 public class CorporateCustomer extends Customer {
 	public String companyName;
 	public int taxNumber;
+
+	public String getCompanyName() {
+		return this.companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public int getTaxNumber() {
+		return this.taxNumber;
+	}
+
+	public void setTaxNumber(int taxNumber) {
+		this.taxNumber = taxNumber;
+	}
 }
