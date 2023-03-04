08a4c3d23bcd926e88670c0b9030d83fbb7571b3
FurkanBaysan <furkanbaysan1600166@gmail.com>
2023-03-04 18:19:14 +0300

    Polymorphism, Dependency Injection with logging scenario (encapsulation support added)

--- inheritancePart2/src/inheritancePart2/IndividualCustomer.java
+++ inheritancePart2/src/inheritancePart2/IndividualCustomer.java
@@ -1,8 +1,40 @@
 package inheritancePart2;
 
 public class IndividualCustomer extends Customer {
-	public String nationalIdentity;
-	public String firstName;
-	public String lastName;
-	public int birthDate;
+    public String nationalIdentity;
+    public String firstName;
+    public String lastName;
+    public int birthDate;
+
+    public String getNationalIdentity() {
+        return this.nationalIdentity;
+    }
+
+    public void setNationalIdentity(String nationalIdentity) {
+        this.nationalIdentity = nationalIdentity;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setBirthDate(int birthDate) {
+        this.birthDate = birthDate;
+    }
 }
